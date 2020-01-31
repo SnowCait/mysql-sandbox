0c25d4b7adc3d41d758261d91b82a8a2c35d003d
雪猫 <SnowCait@users.noreply.github.com>
2020-01-31 22:10:58 +0900

    Update create-tables.sql

--- create-tables.sql
+++ create-tables.sql
@@ -16,3 +16,13 @@ CREATE TABLE `twitter_users` (
   FOREIGN KEY (`player_id`) # 8 bytes
     REFERENCES `players`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `log_player_names` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, # 8 bytes
+  `player_id` BIGINT UNSIGNED NOT NULL, # 8 bytes
+  `name` VARCHAR(20) NOT NULL, # L+1 bytes
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, # 5 bytes
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`player_id`) # 8 bytes
+    REFERENCES `players`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
