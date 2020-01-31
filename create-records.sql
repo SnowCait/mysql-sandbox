8a7fcdf6f1da9c5f4cca70c225a0cc7378461986
雪猫 <SnowCait@users.noreply.github.com>
2020-01-31 22:14:19 +0900

    Update create-records.sql

--- create-records.sql
+++ create-records.sql
@@ -1,6 +1,8 @@
 BEGIN;
 
 INSERT INTO `players` (`name`) VALUES ('user1');
-INSERT INTO `twitter_users` (`id`, `screen_name`, `player_id`) VALUES (123, 'user1', LAST_INSERT_ID());
+SET @player_id = LAST_INSERT_ID();
+INSERT INTO `twitter_users` (`id`, `screen_name`, `player_id`) VALUES (123, 'user1', @player_id);
+INSERT INTO `twitter_users` (`id`, `screen_name`, `player_id`) VALUES (123, 'user1', @player_id);
 
 COMMIT;
