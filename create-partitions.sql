b5c7490ed5493591b7833c590b6bcd62ff834959
SnowCait <SnowCait@users.noreply.github.com>
2020-02-01 05:58:27 +0900

    PK

--- create-partitions.sql
+++ create-partitions.sql
@@ -1,7 +1,13 @@
 -- パーティションと外部キーは同時に定義できないので外部キーを削除する
 -- 消さずに実行すると下記のエラーが出る
 -- ERROR 1506 (HY000) at line 1: Foreign keys are not yet supported in conjunction with partitioning
-ALTER TABLE `log_player_names` DROP FOREIGN KEY `log_player_names_ibfk_1`;
+ALTER TABLE `log_player_names`
+  DROP FOREIGN KEY `log_player_names_ibfk_1`;
+
+-- パーティションのカラムは主キーに含まれないといけない
+ALTER TABLE `log_player_names`
+  DROP PRIMARY KEY,
+  ADD PRIMARY KEY (`id`, `created_at`);
 
 ALTER TABLE `log_player_names` PARTITION BY RANGE COLUMNS(`created_at`) (
   PARTITION p202001 VALUES LESS THAN ('2020-02-01'),
