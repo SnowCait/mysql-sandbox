64ef8c6461f315be8102e21a8080fa35c8044679
SnowCait <SnowCait@users.noreply.github.com>
2020-02-01 05:16:52 +0900

    Update

--- create-records.sql
+++ create-records.sql
@@ -13,7 +13,7 @@ BEGIN;
 SET @player_name = 'user2_from_line';
 INSERT INTO `players` (`name`) VALUES (@player_name);
 SET @player_id = LAST_INSERT_ID();
-INSERT INTO `line_users` (`id`, `player_id`) VALUES (123, @player_id);
+INSERT INTO `line_users` (`id`, `player_id`) VALUES (456, @player_id);
 INSERT INTO `log_player_names` (`player_id`, `name`) VALUES (@player_id, @player_name);
 
 COMMIT;
