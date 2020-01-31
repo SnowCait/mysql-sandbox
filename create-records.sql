f6eb1ea2e02d84b2627bdfa8ccde95aceb87e6be
SnowCait <SnowCait@users.noreply.github.com>
2020-02-01 05:15:28 +0900

    LINE User

--- create-records.sql
+++ create-records.sql
@@ -1,9 +1,19 @@
 BEGIN;
 
-SET @player_name = 'user1';
+SET @player_name = 'user1_from_twitter';
 INSERT INTO `players` (`name`) VALUES (@player_name);
 SET @player_id = LAST_INSERT_ID();
-INSERT INTO `twitter_users` (`id`, `screen_name`, `player_id`) VALUES (123, @player_name, @player_id);
+INSERT INTO `twitter_users` (`id`, `player_id`) VALUES (123, @player_id);
+INSERT INTO `log_player_names` (`player_id`, `name`) VALUES (@player_id, @player_name);
+
+COMMIT;
+
+BEGIN;
+
+SET @player_name = 'user2_from_line';
+INSERT INTO `players` (`name`) VALUES (@player_name);
+SET @player_id = LAST_INSERT_ID();
+INSERT INTO `line_users` (`id`, `player_id`) VALUES (123, @player_id);
 INSERT INTO `log_player_names` (`player_id`, `name`) VALUES (@player_id, @player_name);
 
 COMMIT;
--- create-tables.sql
+++ create-tables.sql
@@ -8,7 +8,16 @@ CREATE TABLE `players` (
 
 CREATE TABLE `twitter_users` (
   `id` BIGINT UNSIGNED NOT NULL, # 8 bytes
-  `screen_name` VARCHAR(15) NOT NULL, # L+1 bytes
+  `player_id` BIGINT UNSIGNED NOT NULL, # 8 bytes
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, # 5 bytes
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, # 5 bytes
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`player_id`) # 8 bytes
+    REFERENCES `players`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `line_users` (
+  `id` BIGINT UNSIGNED NOT NULL, # 8 bytes
   `player_id` BIGINT UNSIGNED NOT NULL, # 8 bytes
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, # 5 bytes
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, # 5 bytes
