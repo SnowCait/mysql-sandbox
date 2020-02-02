a9776475ade48049820ee28202dab877a6c9de06
SnowCait <SnowCait@users.noreply.github.com>
2020-02-03 04:35:40 +0900

    Query

--- generate-dao.php
+++ generate-dao.php
@@ -2,10 +2,20 @@
 $host = 'localhost';
 $dbname = 'sandbox';
 $user = 'root';
-$password = 'root';
-$dsn = "mysql:dbname={$dbname};host={$host}";
+$password = '';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+];
+$dsn = "mysql:dbname={$dbname};host={$host};charset=utf8";
 
-$pdo = new PDO($dsn, $user, $password);
+$pdo = new PDO($dsn, $user, $password, $options);
 foreach($pdo->query('SHOW TABLES') as $row) {
+    var_dump($row->Tables_in_sandbox);
+}
+$stmt = $pdo->prepare('SELECT * FROM `friendships` WHERE `following` = :following');
+$stmt->bindValue(':following', 1, PDO::PARAM_INT);
+$stmt->execute();
+foreach($stmt as $row) {
     var_dump($row);
 }
