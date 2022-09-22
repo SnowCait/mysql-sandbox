b303193080e4a283b116adef04e1ff0469ec31f2
SnowCait <example@example.com>
2022-09-22 16:49:07 +0000

    dump.sql

--- dump.sql
+++ dump.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
 --
 -- Host: localhost    Database: sandbox
 -- ------------------------------------------------------
--- Server version	5.7.31-0ubuntu0.18.04.1
+-- Server version	8.0.30-0ubuntu0.20.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,15 +21,15 @@
 
 DROP TABLE IF EXISTS `friendships`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `friendships` (
-  `following` bigint(20) unsigned NOT NULL,
-  `followed` bigint(20) unsigned NOT NULL,
+  `following` bigint unsigned NOT NULL,
+  `followed` bigint unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`following`,`followed`),
   KEY `following` (`following`,`created_at`),
   KEY `followed` (`followed`,`created_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,22 +38,19 @@ CREATE TABLE `friendships` (
 
 LOCK TABLES `friendships` WRITE;
 /*!40000 ALTER TABLE `friendships` DISABLE KEYS */;
-INSERT INTO `friendships` VALUES (1,2,'2020-10-10 08:21:48'),(1,3,'2020-10-10 08:21:48'),(2,1,'2020-10-10 08:21:48');
+INSERT INTO `friendships` VALUES (1,2,'2022-09-22 16:49:04'),(1,3,'2022-09-22 16:49:05'),(2,1,'2022-09-22 16:49:05');
 /*!40000 ALTER TABLE `friendships` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `logging_friendships_inserted`
-  AFTER INSERT ON `friendships`
-  FOR EACH ROW
-  BEGIN
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `logging_friendships_inserted` AFTER INSERT ON `friendships` FOR EACH ROW BEGIN
     INSERT INTO `log_friendships` (`following`, `followed`, `action`)
       VALUES (NEW.`following`, NEW.`followed`, 1);
   END */;;
@@ -65,16 +62,13 @@ DELIMITER ;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `logging_friendships_deleted`
-  AFTER DELETE ON `friendships`
-  FOR EACH ROW
-  BEGIN
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `logging_friendships_deleted` AFTER DELETE ON `friendships` FOR EACH ROW BEGIN
     INSERT INTO `log_friendships` (`following`, `followed`, `action`)
       VALUES (OLD.`following`, OLD.`followed`, 2);
   END */;;
@@ -90,16 +84,16 @@ DELIMITER ;
 
 DROP TABLE IF EXISTS `line_accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `line_accounts` (
-  `id` bigint(20) unsigned NOT NULL,
-  `user_id` bigint(20) unsigned NOT NULL,
+  `id` bigint unsigned NOT NULL,
+  `user_id` bigint unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `line_accounts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -108,7 +102,7 @@ CREATE TABLE `line_accounts` (
 
 LOCK TABLES `line_accounts` WRITE;
 /*!40000 ALTER TABLE `line_accounts` DISABLE KEYS */;
-INSERT INTO `line_accounts` VALUES (456,2,'2020-10-10 08:21:48','2020-10-10 08:21:48');
+INSERT INTO `line_accounts` VALUES (456,2,'2022-09-22 16:49:04','2022-09-22 16:49:04');
 /*!40000 ALTER TABLE `line_accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,16 +112,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `log_friendships`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `log_friendships` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `following` bigint(20) unsigned NOT NULL,
-  `followed` bigint(20) unsigned NOT NULL,
-  `action` tinyint(3) unsigned NOT NULL,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `following` bigint unsigned NOT NULL,
+  `followed` bigint unsigned NOT NULL,
+  `action` tinyint unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`,`created_at`),
   KEY `followed` (`followed`,`following`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3
 /*!50500 PARTITION BY RANGE  COLUMNS(created_at)
 (PARTITION p202001 VALUES LESS THAN ('2019-02-01') ENGINE = InnoDB,
  PARTITION p202002 VALUES LESS THAN ('2019-03-01') ENGINE = InnoDB,
@@ -141,7 +135,7 @@ CREATE TABLE `log_friendships` (
 
 LOCK TABLES `log_friendships` WRITE;
 /*!40000 ALTER TABLE `log_friendships` DISABLE KEYS */;
-INSERT INTO `log_friendships` VALUES (1,1,2,1,'2020-10-10 08:21:48'),(2,2,1,1,'2020-10-10 08:21:48'),(3,1,3,1,'2020-10-10 08:21:48');
+INSERT INTO `log_friendships` VALUES (1,1,2,1,'2022-09-22 16:49:04'),(2,2,1,1,'2022-09-22 16:49:05'),(3,1,3,1,'2022-09-22 16:49:05');
 /*!40000 ALTER TABLE `log_friendships` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,15 +145,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `log_player_names`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `log_player_names` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) unsigned NOT NULL,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint unsigned NOT NULL,
   `name` varchar(20) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`,`created_at`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3
 /*!50500 PARTITION BY RANGE  COLUMNS(created_at)
 (PARTITION p2020 VALUES LESS THAN ('2021-01-01') ENGINE = InnoDB,
  PARTITION p2021 VALUES LESS THAN ('2022-01-01') ENGINE = InnoDB,
@@ -174,7 +168,7 @@ CREATE TABLE `log_player_names` (
 
 LOCK TABLES `log_player_names` WRITE;
 /*!40000 ALTER TABLE `log_player_names` DISABLE KEYS */;
-INSERT INTO `log_player_names` VALUES (1,1,'user1_from_twitter','2020-10-10 08:21:48'),(2,2,'user2_by_line','2020-10-10 08:21:48'),(3,2,'user2_from_line','2020-10-10 08:21:48'),(4,3,'user3_from_twitter','2020-10-10 08:21:48');
+INSERT INTO `log_player_names` VALUES (1,1,'user1_from_twitter','2022-09-22 16:49:04'),(2,2,'user2_by_line','2022-09-22 16:49:04'),(3,2,'user2_from_line','2022-09-22 16:49:04'),(4,3,'user3_from_twitter','2022-09-22 16:49:04');
 /*!40000 ALTER TABLE `log_player_names` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -184,16 +178,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `tweets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tweets` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) unsigned NOT NULL,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint unsigned NOT NULL,
   `body` json NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `tweets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +196,7 @@ CREATE TABLE `tweets` (
 
 LOCK TABLES `tweets` WRITE;
 /*!40000 ALTER TABLE `tweets` DISABLE KEYS */;
-INSERT INTO `tweets` VALUES (1,1,'{\"text\": \"user1-tweet1\"}','2020-10-10 08:21:48'),(2,1,'{\"text\": \"user1-tweet2\"}','2020-10-10 08:21:48'),(3,1,'{\"text\": \"user1-tweet3\"}','2020-10-10 08:21:48'),(4,2,'{\"text\": \"user2-tweet1\"}','2020-10-10 08:21:48'),(5,2,'{\"text\": \"user2-tweet2\"}','2020-10-10 08:21:48'),(6,2,'{\"text\": \"user2-tweet3\"}','2020-10-10 08:21:48'),(7,3,'{\"text\": \"user3-tweet1\"}','2020-10-10 08:21:48'),(8,3,'{\"text\": \"user3-tweet2\"}','2020-10-10 08:21:48'),(9,3,'{\"text\": \"user3-tweet3\"}','2020-10-10 08:21:48');
+INSERT INTO `tweets` VALUES (1,1,'{\"text\": \"user1-tweet1\"}','2022-09-22 16:49:05'),(2,1,'{\"text\": \"user1-tweet2\"}','2022-09-22 16:49:05'),(3,1,'{\"text\": \"user1-tweet3\"}','2022-09-22 16:49:05'),(4,2,'{\"text\": \"user2-tweet1\"}','2022-09-22 16:49:05'),(5,2,'{\"text\": \"user2-tweet2\"}','2022-09-22 16:49:05'),(6,2,'{\"text\": \"user2-tweet3\"}','2022-09-22 16:49:05'),(7,3,'{\"text\": \"user3-tweet1\"}','2022-09-22 16:49:05'),(8,3,'{\"text\": \"user3-tweet2\"}','2022-09-22 16:49:05'),(9,3,'{\"text\": \"user3-tweet3\"}','2022-09-22 16:49:05');
 /*!40000 ALTER TABLE `tweets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -212,16 +206,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `twitter_accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `twitter_accounts` (
-  `id` bigint(20) unsigned NOT NULL,
-  `user_id` bigint(20) unsigned NOT NULL,
+  `id` bigint unsigned NOT NULL,
+  `user_id` bigint unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `twitter_accounts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -230,7 +224,7 @@ CREATE TABLE `twitter_accounts` (
 
 LOCK TABLES `twitter_accounts` WRITE;
 /*!40000 ALTER TABLE `twitter_accounts` DISABLE KEYS */;
-INSERT INTO `twitter_accounts` VALUES (123,1,'2020-10-10 08:21:48','2020-10-10 08:21:48'),(789,3,'2020-10-10 08:21:48','2020-10-10 08:21:48');
+INSERT INTO `twitter_accounts` VALUES (123,1,'2022-09-22 16:49:04','2022-09-22 16:49:04'),(789,3,'2022-09-22 16:49:04','2022-09-22 16:49:04');
 /*!40000 ALTER TABLE `twitter_accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -240,14 +234,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -256,7 +250,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'user1_from_twitter','2020-10-10 08:21:48','2020-10-10 08:21:48'),(2,'user2_from_line','2020-10-10 08:21:48','2020-10-10 08:21:48'),(3,'user3_from_twitter','2020-10-10 08:21:48','2020-10-10 08:21:48');
+INSERT INTO `users` VALUES (1,'user1_from_twitter','2022-09-22 16:49:04','2022-09-22 16:49:04'),(2,'user2_from_line','2022-09-22 16:49:04','2022-09-22 16:49:04'),(3,'user3_from_twitter','2022-09-22 16:49:04','2022-09-22 16:49:04');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -269,4 +263,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-10  8:21:49
+-- Dump completed on 2022-09-22 16:49:07
